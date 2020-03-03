45e729a8a9eb1ade20321f2d919cdabb9f6e1d9e
aumeisters <54903608+aumeisters@users.noreply.github.com>
2020-03-03 15:07:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,7 @@
     console.time('overallTime');
     console.time('3 sec timer');
     await (new Timer(3)).start
-    .then (  ()=> {
-    console.log('1) 3 sec') || console.timeEnd('3 sec timer') || console.time('2 sec timer')})
+    console.log('1) 3 sec') || console.timeEnd('3 sec timer') || console.time('2 sec timer')
     await (new Timer(2)).start 
-    .then (  ()=> {console.log('2) 2 sec') || console.timeEnd('2 sec timer') || console.timeEnd('overallTime')})
+    console.log('2) 2 sec') || console.timeEnd('2 sec timer') || console.timeEnd('overallTime')
 })();
